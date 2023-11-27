eb3ce2218dd7be6bc0bc47018255943475fd8d78
c7934597 <c7934597@gmail.com>
2023-11-27 18:33:17 +0800

    update

--- api_server/web/application.py
+++ api_server/web/application.py
@@ -2,6 +2,9 @@ from importlib import metadata
 
 from fastapi import FastAPI
 from fastapi.responses import UJSONResponse
+from fastapi.middleware.cors import CORSMiddleware
+
+from debug_toolbar.middleware import DebugToolbarMiddleware
 
 from api_server.logging import configure_logging
 from api_server.web.api.router import api_router
@@ -19,11 +22,22 @@ def get_app() -> FastAPI:
     configure_logging()
     app = FastAPI(
         title="api_server",
-        version=metadata.version("api_server"),
+        description="api_server",
+        version=metadata.version("0.0.1"),
         docs_url="/api/docs",
         redoc_url="/api/redoc",
         openapi_url="/api/openapi.json",
         default_response_class=UJSONResponse,
+        debug=True,
+    )
+
+    app.add_middleware(
+        DebugToolbarMiddleware,
+        CORSMiddleware,
+        allow_origins=["*"],
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
     )
 
     # Adds startup and shutdown events.
